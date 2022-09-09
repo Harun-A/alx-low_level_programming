d9188441bc23f7565a8849369fd44ee611da8e5c
harun abubakar siddique <harun.abubakarsiddique@gmail.com>
2022-09-09 15:41:53 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int outter_num;
+	int inner_num;
+
+	for (outter_num = '0'; outter_num <= '9'; outter_num++)
+	{
+		for (inner_num = (outter_num + 1); inner_num <= '9'; inner_num++)
+		{
+			putchar(outter_num);
+			putchar(inner_num);
+
+			if (outter_num != '8' || inner_num != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}	
